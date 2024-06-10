7ae30ee46e9d3941fa2c2f311ee78c2504e5e795
Kamit <timaplay137@gmail.com>
2024-06-10 22:14:52 +0300

    sql сосааааать

--- sublime/pyt.py
+++ sublime/pyt.py
@@ -7,13 +7,20 @@ bot = telebot.TeleBot('6744998769:AAE0_KtJNM9ehs4hu535LyJJa31T0SdyNY8')
 
 
 
+
+
+
+
+
+
+
 # действие команд
 # Большие кнопки
 @bot.message_handler(commands=['start'])
 def chat(message):
     markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
     b5 = (types.KeyboardButton('ы'))
-    b6 = (types.KeyboardButton('Внести параметры замера'))
+    b6 = (types.KeyboardButton('Новый замер'))
     b7 = (types.KeyboardButton('Меню'))
 
     markup1.row(b5,b6,b7)
@@ -35,11 +42,17 @@ def callback_message(callback):
     if callback.data == 'delete':
         bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
         bot.edit_message_text("Удалено", callback.message.chat.id, callback.message.message_id)
-    elif callback.data == 'edit':
-        bot.edit_message_text("Edit text", callback.message.chat.id, callback.message.message_id)
     elif callback.data == 'exit':
         bot.delete_message(callback.message.chat.id, callback.message.message_id)
         bot.delete_message(callback.message.chat.id, callback.message.message_id-1)
+    elif callback.data == "body":
+        bot.send_message(callback.message.chat.id, "Введите ваш возраст или обычное давление, отличающееся от средних табличных значений(пример:120/80): ")
+        photo = open('/home/kamit/Документы/GitHub/pressure/sublime/7a51f5eb7fe32122f38a0a4c1ad38639.jpeg','rb')
+        bot.send_photo(callback.message.chat.id, photo)
+        user_age()
+
+
+
 
 # Ответ на определённые фразы юзера
 @bot.message_handler()
@@ -52,6 +65,8 @@ def info(message):
         bot.send_message(message.chat.id, 'Удалено')
     elif message.text == "Меню":
         menu(message)
+    
+
     else:
         bot.send_message(message.chat.id, 'Cам ты '+(message.text) )
 
@@ -67,17 +82,46 @@ def menu(message):
 
     b1 = (types.InlineKeyboardButton('Закрыть меню',callback_data='exit'))
     b2 = (types.InlineKeyboardButton('Новый замер давления',callback_data='new_pressure'))
-    b3 = (types.InlineKeyboardButton('Внести/редактировать данные о теле',callback_data='s'))
+    b3 = (types.InlineKeyboardButton('Внести/редактировать данные о теле',callback_data='body'))
 
-    b4 = (types.InlineKeyboardButton('Статистика',callback_data='s'))
+    b4 = (types.InlineKeyboardButton('Статистика',callback_data='full_data'))
 
     markup2.add(b3,b2,b4,b1)
     
     bot.send_message(message.chat.id, '\nВыберите функцию:', reply_markup=markup2)
 
 
+
+
+
+
+
+
+#
+def user_age():
+    
+    age = 10
+    db = sqlite3.connect('power.db')
+
+
+    c = db.cursor()
+
+    c.execute("""CREATE TABLE IF NOT EXISTS pressure(age text)""")
+
+
+
+    c.execute("INSERT INTO pressure(age)VALUES('%s')"%(age))
+
+    db.commit()
+    db.close()
+
+
+
+
+
+
 # Команда id
-@bot.message_handler(commands=['id'])
+@bot.message_handler(commands=['id'])   
 def chat(message):
     bot.reply_to(message, f"Ваш ID:{message.from_user.id}")
 
--- sublime/sql.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-db = sqlite3.connect('power.db')
-
-c = db.cursor()
-
-c.execute("""CREATE TABLE pressure(name text, age text, normal pressure integer, pressure_of_pepople integer)""")
-
-db.commit()
-db.close()
