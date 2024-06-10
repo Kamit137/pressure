101a096d0678a6f7808213d6422b2686a8a8420f
Kamit <timaplay137@gmail.com>
2024-06-10 16:18:48 +0300

    ура. работает менюшка

--- sublime/pyt.py
+++ sublime/pyt.py
@@ -3,17 +3,7 @@ import sqlite3
 from telebot import types
 
 bot = telebot.TeleBot('6744998769:AAE0_KtJNM9ehs4hu535LyJJa31T0SdyNY8')
-#fdff
-#dfd
-#ssss
-#ssss
-#ssss
-#ssss
-#ssss
-#ssss
-#ssss
-#ssss
-#ssss
+
 
 
 
@@ -21,13 +11,15 @@ bot = telebot.TeleBot('6744998769:AAE0_KtJNM9ehs4hu535LyJJa31T0SdyNY8')
 # Большие кнопки
 @bot.message_handler(commands=['start'])
 def chat(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    b1 = (types.KeyboardButton('Ссылка хрен пойми куда'))
-    b3 = (types.KeyboardButton('Получить мем'))
-    markup.add(b1, b3)
-    bot.send_message(message.chat.id, 'Привет', reply_markup=markup)
-    # bot.register_next_step_handler(message, on_click)
+    markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    b5 = (types.KeyboardButton('Ссылка хрен пойми куда'))
+    b6 = (types.KeyboardButton('Внести параметры замера'))
+    b7 = (types.KeyboardButton('Меню'))
 
+    markup1.row(b5,b6,b7)
+    bot.send_message(message.chat.id, 'Добрый день, этот бот поможет вам следить за здоровьем!',reply_markup=markup1)
+    menu(message)
+    
 
 # Команда id
 @bot.message_handler(commands=['id'])
@@ -57,7 +49,8 @@ def callback_message(callback):
         bot.edit_message_text("Удалено", callback.message.chat.id, callback.message.message_id)
     elif callback.data == 'edit':
         bot.edit_message_text("Edit text", callback.message.chat.id, callback.message.message_id)
-
+    elif callback.data == 'exit':
+        bot.delete_message(callback.message.chat.id, callback.message.message_id - 0)
 
 # Ответ на определённые фразы юзера
 @bot.message_handler()
@@ -67,18 +60,39 @@ def info(message):
     elif message.text.lower() == "id":
         bot.reply_to(message, f"Ваш ID:{message.from_user.id}")
 
-    if message.text == "Ссылка хрен пойми куда":
-        bot.send_message(message.chat.id,
-                         "https://www.youtube.com/watch?v=RpiWnPNTeww&list=PL0lO_mIqDDFUev1gp9yEwmwcy8SicqKbt&index=4")
+    elif message.text == "Ссылка хрен пойми куда":
+        bot.send_message(message.chat.id,"https://www.youtube.com/watch?v=RpiWnPNTeww&list=PL0lO_mIqDDFUev1gp9yEwmwcy8SicqKbt&index=4")
     elif message.text == "Удалить":
         bot.send_message(message.chat.id, 'Удалено')
+
+    elif message.text == "Меню":
+
+        
+        menu(message)
+       
+
     else:
-        #bot.send_message(message.chat.id, 'Сам ты')
-        
-        
         bot.send_message(message.chat.id, 'Cам ты '+(message.text) )
 
 
-
+    
 # Чтобы бот работал многоразово
+
+
+def menu(message):
+
+
+    markup2 = types.InlineKeyboardMarkup( row_width=2)
+
+    b1 = (types.InlineKeyboardButton('закрыть',callback_data='exit'))
+    b2 = (types.InlineKeyboardButton('Внести свои данные',callback_data='s'))
+ 
+    b4 = (types.InlineKeyboardButton('Статистика',callback_data='s'))
+
+    markup2.add(b1,b2,b4)
+    
+    bot.send_message(message.chat.id, '\nВыберите фунцию:', reply_markup=markup2)
+
+
+
 bot.polling(none_stop=True)
--- sublime/sql.py
+++ sublime/sql.py
@@ -3,7 +3,7 @@ db = sqlite3.connect('power.db')
 
 c = db.cursor()
 
-#c.execute("""CREATE TABLE art(title text, full_text text, viw integer)""")
+c.execute("""CREATE TABLE pressure(name text, age text, normal pressure integer, pressure_of_pepople integer)""")
 
 db.commit()
 db.close()
