84d5fac2baa9207c1075f8ef616ffd1550b11ff1
Kamit <timaplay137@gmail.com>
2024-06-10 16:47:19 +0300

    m

--- sublime/pyt.py
+++ sublime/pyt.py
@@ -12,36 +12,24 @@ bot = telebot.TeleBot('6744998769:AAE0_KtJNM9ehs4hu535LyJJa31T0SdyNY8')
 @bot.message_handler(commands=['start'])
 def chat(message):
     markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    b5 = (types.KeyboardButton('Ссылка хрен пойми куда'))
+    b5 = (types.KeyboardButton('ы'))
     b6 = (types.KeyboardButton('Внести параметры замера'))
     b7 = (types.KeyboardButton('Меню'))
 
     markup1.row(b5,b6,b7)
     bot.send_message(message.chat.id, 'Добрый день, этот бот поможет вам следить за здоровьем!',reply_markup=markup1)
     menu(message)
-    
-
-# Команда id
-@bot.message_handler(commands=['id'])
-def chat(message):
-    bot.reply_to(message, f"Ваш ID:{message.from_user.id}")
-
-
 # Если человек прислал фото
 @bot.message_handler(content_types=['photo'])
 def get_photo(message):
     marcup = types.InlineKeyboardMarkup()
-    b1 = (types.InlineKeyboardButton('Ссылка хрен пойми куда',
-                                     url='https://www.youtube.com/watch?v=RpiWnPNTeww&list=PL0lO_mIqDDFUev1gp9yEwmwcy8SicqKbt&index=4'))
+    b1 = (types.InlineKeyboardButton('Ссылка хрен пойми куда',url='https://youtu.be/dQw4w9WgXcQ'))
     b2 = (types.InlineKeyboardButton('Удалить', callback_data='delete'))
     marcup.row(b1, b2)
-
-    b3 = (types.InlineKeyboardButton('Изменить', callback_data='edit'))
-    marcup.row(b3)
     bot.reply_to(message, 'Достойно', reply_markup=marcup)
 
 
-# Настройки для кнопок
+#Ответки кнопок
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
     if callback.data == 'delete':
@@ -50,7 +38,8 @@ def callback_message(callback):
     elif callback.data == 'edit':
         bot.edit_message_text("Edit text", callback.message.chat.id, callback.message.message_id)
     elif callback.data == 'exit':
-        bot.delete_message(callback.message.chat.id, callback.message.message_id - 0)
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+        bot.delete_message(callback.message.chat.id, callback.message.message_id-1)
 
 # Ответ на определённые фразы юзера
 @bot.message_handler()
@@ -59,40 +48,38 @@ def info(message):
         bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
     elif message.text.lower() == "id":
         bot.reply_to(message, f"Ваш ID:{message.from_user.id}")
-
-    elif message.text == "Ссылка хрен пойми куда":
-        bot.send_message(message.chat.id,"https://www.youtube.com/watch?v=RpiWnPNTeww&list=PL0lO_mIqDDFUev1gp9yEwmwcy8SicqKbt&index=4")
     elif message.text == "Удалить":
         bot.send_message(message.chat.id, 'Удалено')
-
     elif message.text == "Меню":
-
-        
         menu(message)
-       
-
     else:
         bot.send_message(message.chat.id, 'Cам ты '+(message.text) )
 
 
     
-# Чтобы бот работал многоразово
 
 
+#Вызов меню
 def menu(message):
 
 
-    markup2 = types.InlineKeyboardMarkup( row_width=2)
+    markup2 = types.InlineKeyboardMarkup(row_width=1)
 
-    b1 = (types.InlineKeyboardButton('закрыть',callback_data='exit'))
-    b2 = (types.InlineKeyboardButton('Внести свои данные',callback_data='s'))
- 
+    b1 = (types.InlineKeyboardButton('Закрыть меню',callback_data='exit'))
+    b2 = (types.InlineKeyboardButton('Новый замер давления',callback_data='new_pressure'))
+    b3 = (types.InlineKeyboardButton('Внести/редактировать данные о теле',callback_data='s'))
+
     b4 = (types.InlineKeyboardButton('Статистика',callback_data='s'))
 
-    markup2.add(b1,b2,b4)
+    markup2.add(b3,b2,b4,b1)
     
-    bot.send_message(message.chat.id, '\nВыберите фунцию:', reply_markup=markup2)
+    bot.send_message(message.chat.id, '\nВыберите функцию:', reply_markup=markup2)
 
 
+# Команда id
+@bot.message_handler(commands=['id'])
+def chat(message):
+    bot.reply_to(message, f"Ваш ID:{message.from_user.id}")
 
+# Чтобы бот работал многоразово
 bot.polling(none_stop=True)
